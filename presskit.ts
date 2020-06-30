3590cd887e5fbcba5bb5bd36f4cda8ea65b29d6e
treets <mail@andreas-theis.net>
2020-06-30 08:07:50 +0200

    first changes

---
+++ presskit.ts
@@ -0,0 +1,8 @@
+namespace presskit {
+    //% block="move to initial $position with $velocity and $acceleration || max force $overload" 
+    //% velocity.defl=100 acceleration.defl=100
+    //% inlineInputMode=inline
+    export function initialPosition(velocity: number, acceleration: number, position: number, overload: number): void {
+        console.log('presskit.initialPosition')
+    }
+}
