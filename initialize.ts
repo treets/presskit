3819b34b4defb959eb468e2ae2e654b6ee000b89
treets <mail@andreas-theis.net>
2020-07-22 13:32:56 +0200

    Update pxt.json, main.blocks, initialize.ts

---
+++ initialize.ts
@@ -0,0 +1,6 @@
+namespace that.initialize {
+    //% block="initialize mqtt || $config" blockHidden=1
+    export function run(config: any): void {
+        console.log('that.initialize.run()...');
+    }
+}
